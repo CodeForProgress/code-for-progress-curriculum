ef44c7fb94d4f3fbf5ddd0682fa5fa3cca7c3184
aliyarahman <aliya.rahman@yahoo.com>
2014-01-30 15:54:17 -0500

    Added nav

--- app/views.py
+++ app/views.py
@@ -8,9 +8,18 @@ from app import app, db
 def index():
 	weeks = range(1,17)
 	titles = ["Radical Welcomings"]
-	week1lessontitles = ["The Big Picture: Why us and why now?","Setting Up for (a) Movement: Staying healthy and happy while we learn and work","Master's Tools, Remastered Tools, Native Tools: Coding our first social justice app from things we know",u"Our Compañeros: Meeting our larger community of support", "Relax, Reboot, Reimagine: Visioning and planning the tools we'll build in this program"]
+	week1lessontitles = ["The Big Picture: Why us and why now?","Setting Up for (a) Movement: Staying healthy and happy while we learn and work","Master's Tools, Remastered Tools, Native Tools: Coding our first social justice app from things we already know",u"Our Compañeros: Meeting our larger community of support", "Relax, Reboot, Reimagine: Visioning and planning the tools we'll build in this program"]
 	return render_template("index.html", weeks = weeks, title = titles, week1lessontitles = week1lessontitles)
 
 @app.route('/lessons/<shortcode>', methods = ['GET', 'POST'])
 def lesson(shortcode):
-	return render_template("lesson_plan.html")
+	if shortcode == "1":
+		return render_template("lessonplan0101.html", shortcode=shortcode)
+	if shortcode == "2":
+		return render_template("lessonplan0102.html", shortcode=shortcode)
+	if shortcode == "3":
+		return render_template("lessonplan0103.html", shortcode=shortcode)
+	if shortcode == "4":
+		return render_template("lessonplan0104.html", shortcode=shortcode)
+	if shortcode == "5":
+		return render_template("lessonplan0105.html", shortcode=shortcode)
